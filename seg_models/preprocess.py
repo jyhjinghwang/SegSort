4ea45a1636d231d57274e71a507b780cee74ea1f
paritosh5feb <86036719+paritosh5feb@users.noreply.github.com>
2021-08-31 11:36:02 +0530

    create_image_tensor() function implemented.

--- seg_models/preprocess.py
+++ seg_models/preprocess.py
@@ -15,3 +15,29 @@ code base, using bash scripts, some changes might be required in the bash script
  
 '''
 
+'''
+create_image_tensor() function reads images (train & test) and creates a tensor of dimensions [Batch Size, Width, Height, Channels]
+to be fed into the image_reader function of SegSort. This function saves the tensor in the .npy format
+
+Args:
+     path: string indicating the path to the directory containing train and test folders.
+Returns:
+     x_train: A 4D Tensor of dimensions [Batch Size, Width, Height, Channels] containing training images.
+     x_test:  A 4D Tensor of dimensions [Batch Size, Width, Height, Channels] containing training images.
+
+'''
+
+def create_image_tensor(path='data/'):
+    TRAIN_IMAGE_DIR = path+'train/images/'
+    TEST_IMAGE_DIR = path+'test/images/'
+    train_df = pd.read_csv(path+'train.csv')
+    test_df = pd.read_csv(path+'sample_submission.csv')
+    train_imgs = [load_img(TRAIN_IMAGE_DIR + image_name + '.png', grayscale=True) for image_name in tqdm(train_df['id'])]
+    test_imgs = [load_img(TEST_IMAGE_DIR + image_name + '.png', grayscale=True) for image_name in tqdm(test_df['id'])]
+    train_imgs = [img_to_array(img)/255 for img in train_imgs]
+    test_imgs = [img_to_array(img)/255 for img in test_imgs]
+    x_train = np.array(train_imgs)
+    x_test = np.array(test_imgs)
+    np.save("x_train_images.npy", x_train)
+    np.save("x_test_images.npy", x_test)
+    return x_train, x_test
